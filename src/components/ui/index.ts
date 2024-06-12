9f655a880719b8fbb868964c52abbd8cfa734544
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-12 23:51:08 +0530

    Add unified exports for UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Card } from "./Card";
+export { Input } from "./Input";
+export { Label } from "./Label";
+export { Icons } from "./Icons";
+export { Button } from "./Button";
+export { Separator } from "./Separator";
+export { Toaster } from "./Toaster";
