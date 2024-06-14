c3ec011dcbfebd5091e03f1a796a3da06365155f
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-14 21:21:18 +0530

    Add central export file for shared components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Phone } from "./Phone";
+export { default as Navbar } from "./Navbar";
+export { default as Providers } from "./Providers";
+export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
