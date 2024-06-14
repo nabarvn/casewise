8a44ab967db489cbcddc8af8a9a43cf75201e909
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-14 21:21:21 +0530

    Add Clerk Middleware to protect and redirect routes

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { absoluteUrl } from "@/lib/utils";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+
+  if (
+    (req.url.includes("/sign-in") || req.url.includes("/sign-up")) &&
+    !!auth().userId
+  ) {
+    return Response.redirect(absoluteUrl("/"));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
