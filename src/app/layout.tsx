5e2ddbeacd8a3132ebe5e12fa1acdc0ab3d34b17
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-21 19:13:59 +0530

    Enhance layout with Providers and Toaster integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Providers } from "@/components";
+import { Toaster } from "@/components/ui";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="hydrated antialiased">
+      <Providers>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </Providers>
     </html>
   );
 }
