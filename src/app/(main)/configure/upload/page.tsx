e6bb62414995e1998f7a9e462f356fc464178ed6
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-24 19:28:26 +0530

    Add image upload page with drag-and-drop support

---
+++ src/app/(main)/configure/upload/page.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Progress } from "@/components/ui";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { useState, useTransition } from "react";
+import { useUploadThing } from "@/lib/uploadthing";
+import Dropzone, { FileRejection } from "react-dropzone";
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+
+const UploadPage = () => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const [isPending, startTransition] = useTransition();
+
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId;
+
+      startTransition(() => {
+        router.push(`/configure/design?id=${configId}`);
+      });
+    },
+    onUploadProgress(p) {
+      setUploadProgress(p);
+    },
+  });
+
+  const onDropAccepted = (acceptedFiles: File[]) => {
+    startUpload(acceptedFiles, { configId: undefined });
+    setIsDragOver(false);
+  };
+
+  const onDropRejected = (rejectedFiles: FileRejection[]) => {
+    const [file] = rejectedFiles;
+    setIsDragOver(false);
+
+    toast({
+      title: `"${file.file.type}" type is not supported`,
+      description: "Please choose a PNG, JPG, or JPEG image instead.",
+      variant: "destructive",
+    });
+  };
+
+  return (
+    <div
+      className={cn(
+        "relative my-12 flex h-full w-full flex-1 flex-col items-center justify-center rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:my-16 lg:rounded-2xl",
+        {
+          "bg-blue-900/10 ring-blue-900/25": isDragOver,
+        },
+      )}
+    >
+      <div className="relative flex w-full flex-1 flex-col items-center justify-center">
+        <Dropzone
+          onDropAccepted={onDropAccepted}
+          onDropRejected={onDropRejected}
+          accept={{
+            "image/png": [".png"],
+            "image/jpeg": [".jpeg"],
+            "image/jpg": [".jpg"],
+          }}
+          onDragEnter={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <div
+              className="flex h-full w-full flex-1 flex-col items-center justify-center gap-y-2"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+
+              {isDragOver ? (
+                <MousePointerSquareDashed className="size-6 text-zinc-500" />
+              ) : isUploading || isPending ? (
+                <Loader2 className="size-6 animate-spin text-zinc-500" />
+              ) : (
+                <Image className="size-6 text-zinc-500 xl:size-8" />
+              )}
+
+              <div className="flex flex-col justify-center text-sm text-zinc-700">
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading...</p>
+
+                    <Progress
+                      value={uploadProgress}
+                      className="mt-2 h-2 w-40 bg-gray-300"
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className="font-semibold">Drop file</span> to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                )}
+              </div>
+
+              {isPending ? null : (
+                <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>
+              )}
+            </div>
+          )}
+        </Dropzone>
+      </div>
+    </div>
+  );
+};
+
+export default UploadPage;
