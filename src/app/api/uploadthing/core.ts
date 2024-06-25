a4db9748598e12650384f3c020f8b45aa3939b4a
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-25 19:50:17 +0530

    Add file upload feature with image processing and DB config

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/server/db";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .input(z.object({ configId: z.string().optional() }))
+    .middleware(async ({ input }) => {
+      // whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { input };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const { configId } = metadata.input;
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+
+        // whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        // whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+        return { configId: updatedConfiguration.id };
+      }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
