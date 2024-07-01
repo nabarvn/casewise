95f5cba51ed3df11c78e8369ce7f246f6a94bd24
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-01 20:02:40 +0530

    Enhance layout styling for better visual consistency

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }: PropsWithChildren) => (
   <Fragment>
     <Navbar />
 
-    <main className="flex min-h-[calc(100svh-136px)] flex-col">
+    <main className="grainy-light dark:grainy-dark flex min-h-[calc(100svh-136px)] flex-col">
       <div className="flex h-full flex-1 flex-col">{children}</div>
     </main>
 
