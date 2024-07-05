a7aea7929c1079867ebea3aa144974ee150d8151
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-05 21:01:12 +0530

    Add route matcher for configuration endpoints

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,10 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+.[w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+    "/configure(.*)",
+  ],
 };
