73c2b26ff994b30c689ff0defe0b390191c5224d
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-05 20:30:04 +0530

    Enhance Providers with Tooltip and React Query support

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,9 +2,17 @@
 
 import { PropsWithChildren } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { TooltipProvider } from "@/components/ui/Tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 const Providers = ({ children }: PropsWithChildren) => (
-  <ClerkProvider>{children}</ClerkProvider>
+  <ClerkProvider>
+    <QueryClientProvider client={client}>
+      <TooltipProvider>{children}</TooltipProvider>
+    </QueryClientProvider>
+  </ClerkProvider>
 );
 
 export default Providers;
