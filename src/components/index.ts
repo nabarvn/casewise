a55326ccba3e674285c00f8b0255a5fd38417c94
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-05 21:02:36 +0530

    Expanded component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,7 @@ export { default as Phone } from "./Phone";
 export { default as Navbar } from "./Navbar";
 export { default as Providers } from "./Providers";
 export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
+export { default as Reviews } from "./Reviews";
+export { default as Footer } from "./Footer";
+export { default as Steps } from "./Steps";
+export { default as LoginModal } from "./LoginModal";
