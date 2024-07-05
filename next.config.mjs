9ec7a53d22e5f3241f765778fd74ea7a55bde8df
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-05 20:57:20 +0530

    Enhance image configuration for remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
