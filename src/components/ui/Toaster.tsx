dbc2168e7ff0b7356d2faa7c3a20a0b351a9e9ef
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-07-25 22:05:32 +0530

    Refactor Toaster component for enhanced toast handling

--- src/components/ui/Toaster.tsx
+++ src/components/ui/Toaster.tsx
@@ -1,31 +1,39 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import { Toaster as Sonner } from "sonner";
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/Toast";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>;
+import { useToast } from "@/hooks/use-toast";
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+export function Toaster() {
+  const { toasts } = useToast();
 
   return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-        },
-      }}
-      {...props}
-    />
-  );
-};
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
 
-export { Toaster };
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+
+            {action}
+            <ToastClose />
+          </Toast>
+        );
+      })}
+
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
