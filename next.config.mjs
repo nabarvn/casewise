2b8974f83f92dbbf4c7ef6d2bc24f801a30f7241
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-01-15 16:32:33 +0530

    Enable fetching images from subdomains of `ufs.sh`

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "utfs.io",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.ufs.sh",
+        pathname: "/**",
+      },
     ],
   },
 };
